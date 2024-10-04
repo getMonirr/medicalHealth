fed7effc0443e5854c3932e6c4fdfb3b53a6e8bc
getMonirr <getMonirr@gmail.com>
2024-10-05 02:30:16 +0600

    update sokect link

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,6 +1,7 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:5000");
+// export const socket = io("http://localhost:5000");
+export const socket = io("https://medical-health-snowy.vercel.app");
 
 socket.on("connection", (socket) => {
   console.log("a user connected");
